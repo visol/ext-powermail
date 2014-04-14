a0bd52db0a30db9553787a27614ffe03db51f4b1
Alex Kellner <alexander.kellner@in2code.de>
2014-04-14 16:35:57 +0200

    Version uipdate

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,10 +35,10 @@ $EM_CONF[$_EXTKEY] = array (
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '6.0.0-6.2.99',
+			'typo3' => '6.2.0-6.2.99',
+			'extbase' => '6.2.0-6.2.99',
+			'fluid' => '6.2.0-6.2.99',
 			'cms' => '',
-			'extbase' => '',
-			'fluid' => '',
 		),
 		'conflicts' => array(),
 		'suggests' => array(),
