6f83676734d437deacdb1ab504dd5307d2e3f6b3
Alex Kellner <alexander.kellner@in2code.de>
2014-05-01 19:55:31 +0200

    jQuery 1.11.0 and Fallback Datepicker with HTML5

--- Resources/Public/JavaScripts/form.js
+++ Resources/Public/JavaScripts/form.js
@@ -30,34 +30,40 @@ jQuery(document).ready(function($) {
 
 	// Datepicker field
 	if ($.fn.datepicker) {
-		$('.powermail_date').datepicker({
-			dateFormat: $('.container_datepicker_dateformat:first').val(),
-			dayNamesMin: [
-				$('.container_datepicker_day_so:first').val(),
-				$('.container_datepicker_day_mo:first').val(),
-				$('.container_datepicker_day_tu:first').val(),
-				$('.container_datepicker_day_we:first').val(),
-				$('.container_datepicker_day_th:first').val(),
-				$('.container_datepicker_day_fr:first').val(),
-				$('.container_datepicker_day_sa:first').val()
-			],
-			monthNames: [
-				$('.container_datepicker_month_jan:first').val(),
-				$('.container_datepicker_month_feb:first').val(),
-				$('.container_datepicker_month_mar:first').val(),
-				$('.container_datepicker_month_apr:first').val(),
-				$('.container_datepicker_month_may:first').val(),
-				$('.container_datepicker_month_jun:first').val(),
-				$('.container_datepicker_month_jul:first').val(),
-				$('.container_datepicker_month_aug:first').val(),
-				$('.container_datepicker_month_sep:first').val(),
-				$('.container_datepicker_month_oct:first').val(),
-				$('.container_datepicker_month_nov:first').val(),
-				$('.container_datepicker_month_dec:first').val()
-			],
-			nextText: '&gt;',
-			prevText: '&lt;',
-			firstDay: 1
+		$('.powermail_date').each(function() {
+			// stop datepicker ui, if browser supports type="date"
+			if ($(this).get(0).type === 'date') {
+				return;
+			}
+			$(this).datepicker({
+				dateFormat: $('.container_datepicker_dateformat:first').val(),
+				dayNamesMin: [
+					$('.container_datepicker_day_so:first').val(),
+					$('.container_datepicker_day_mo:first').val(),
+					$('.container_datepicker_day_tu:first').val(),
+					$('.container_datepicker_day_we:first').val(),
+					$('.container_datepicker_day_th:first').val(),
+					$('.container_datepicker_day_fr:first').val(),
+					$('.container_datepicker_day_sa:first').val()
+				],
+				monthNames: [
+					$('.container_datepicker_month_jan:first').val(),
+					$('.container_datepicker_month_feb:first').val(),
+					$('.container_datepicker_month_mar:first').val(),
+					$('.container_datepicker_month_apr:first').val(),
+					$('.container_datepicker_month_may:first').val(),
+					$('.container_datepicker_month_jun:first').val(),
+					$('.container_datepicker_month_jul:first').val(),
+					$('.container_datepicker_month_aug:first').val(),
+					$('.container_datepicker_month_sep:first').val(),
+					$('.container_datepicker_month_oct:first').val(),
+					$('.container_datepicker_month_nov:first').val(),
+					$('.container_datepicker_month_dec:first').val()
+				],
+				nextText: '&gt;',
+				prevText: '&lt;',
+				firstDay: 1
+			});
 		});
 	}
 });
