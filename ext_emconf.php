d10db08339fa94255cc2cdcb4d5f5ca55ecb0ae7
Alex Kellner <alexander.kellner@in2code.de>
2014-05-11 14:50:52 +0200

    Version update to beta

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,7 +16,7 @@ $EM_CONF[$_EXTKEY] = array (
 		mailform extension with a lots of features (spam prevention, marketing, double-optin, etc...)',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.1.0-alpha',
+	'version' => '2.1.0-beta',
 	'dependencies' => 'cms,extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
