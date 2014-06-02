c00bc4e0adfa0e98f9bb26cb686288df6a37948a
Alex Kellner <alexander.kellner@in2code.de>
2014-06-02 16:20:57 +0200

    Small Security Fix

--- Classes/ViewHelpers/Getter/GetFieldsFromFormViewHelper.php
+++ Classes/ViewHelpers/Getter/GetFieldsFromFormViewHelper.php
@@ -15,13 +15,18 @@ class GetFieldsFromFormViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\Abstr
 	 *
 	 * @param \In2code\Powermail\Domain\Model\Form $form
 	 * @param string $property
+	 * @param bool $htmlSpecialChars
 	 * @return array
 	 */
-	public function render(\In2code\Powermail\Domain\Model\Form $form, $property = 'title') {
+	public function render(\In2code\Powermail\Domain\Model\Form $form, $property = 'title', $htmlSpecialChars = TRUE) {
 		$fields = array();
 		foreach ($form->getPages() as $page) {
 			foreach ($page->getFields() as $field) {
-				$fields[] = $field->{'get' . ucfirst($property)}();
+				$fieldProperty = $field->{'get' . ucfirst($property)}();
+				if ($htmlSpecialChars) {
+					$fieldProperty = htmlspecialchars($fieldProperty);
+				}
+				$fields[] = $fieldProperty;
 			}
 		}
 		return $fields;
